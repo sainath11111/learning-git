b2296fe3d09100ebc354be1394d769cddc96b0cb
krushna <zinjurdesainath@gmail.com>
2026-01-31 10:59:31 +0530

    changes by krushna

--- first.js
+++ first.js
@@ -3,3 +3,7 @@ console.log("bhai kay krtoy")
 console.log("nikhil zoptoy")
 console.log("mi sai ahe ata mi push krtoy")
 console.log("sinath git hun shekavtoy pranva la")
+console.log("bc")
+console.log("mc")
+
+
